a9ba2954ccaebdecb8e07b4598aae44015de535e
Richard S. Brown <richsb007@gmail.com>
2022-07-15 13:09:12 -0400

    updated index.js for React 18 format

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //import reportWebVitals from './reportWebVitals';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createStore, applyMiddleware, compose } from 'redux';
@@ -33,16 +33,16 @@ const store = createStore(persistedReducer,
   const persistor = persistStore(store)
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-    <App />
-    </PersistGate>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.createRoot(document.getElementById('root'))
+  .render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+      <App />
+      </PersistGate>
+      </Provider>
+    </React.StrictMode>,
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
