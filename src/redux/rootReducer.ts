7741f6e2c9573dd4fa8510f3105c253fd6b9a63f
Richard S. Brown <richsb007@gmail.com>
2023-01-10 23:16:02 -0500

    code cleanup

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux";
-import cakeReducer from '../features/cake/cakeSlice';
 import authReducer from '../features/auth/authSlice';
 import taskReducer from '../features/tasks/taskSlice'
 
 const rootReducer = combineReducers({
     auth: authReducer,
     task: taskReducer,
-    cake: cakeReducer
 });
 
 export default rootReducer;
