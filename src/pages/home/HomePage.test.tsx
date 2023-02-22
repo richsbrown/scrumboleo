d33a809aa219c4ca17f195e829ee7ac8696626b7
Richard S. Brown <richsb007@gmail.com>
2023-02-21 23:57:58 -0500

    testing

--- src/pages/home/HomePage.test.tsx
+++ src/pages/home/HomePage.test.tsx
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import HomePage from './HomePage';
 
 test('renders the application name', () => {
+  // test HomePage
   render(<HomePage />);
   const divElement = screen.getByRole("contentinfo");
   expect(divElement).toHaveTextContent("Scrumboleo");
